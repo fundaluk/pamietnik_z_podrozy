c6f0acbb13839f2c2a3b99cf68cc05166dc23efb
mrenok <50719241+mrenok@users.noreply.github.com>
2019-09-03 18:46:06 +0200

    Zmiana nazwy użytkownika #2

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -2,7 +2,20 @@ import * as React from 'react';
 import { useContext, useEffect, useState } from 'react';
 import { RouteComponentProps } from 'react-router-dom';
 
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonGrid, IonRow, IonCol, IonText, IonButton } from '@ionic/react';
+import {
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonText,
+  IonButton,
+  IonItem,
+  IonLabel,
+  IonInput,
+} from '@ionic/react';
 
 import FirebaseContext from '../components/FirebaseContext';
 import UserContext from '../components/UserContext';
@@ -13,6 +26,36 @@ const Profile: React.FunctionComponent<RouteComponentProps> = ({ history }) => {
   const firebase = useContext(FirebaseContext);
   const { user } = useContext(UserContext);
   const { displayName, email, photoURL } = user;
+
+  const [username, setUsername] = useState<string>(displayName);
+  const [usernameValid, setUsernameValid] = useState<boolean>(false);
+  const [isEditingUsername, setIsEditingUsername] = useState<boolean>(false);
+
+  // Sprawdź za każdym razem jak się zmieni username  czy formularz jest poprawny
+  useEffect(() => {
+    if (username !== '' && username.length > 3) {
+      setUsernameValid(true);
+    } else {
+      setUsernameValid(false);
+    }
+  }, [username]);
+
+  const changeUsername = (event: Event) => {
+    event.preventDefault();
+    setIsEditingUsername(!isEditingUsername);
+  };
+
+  const handleUsernameChange = async (event: Event) => {
+    event.preventDefault();
+    if (usernameValid) {
+      try {
+        await firebase.auth.currentUser.updateProfile({ displayName: username });
+        setIsEditingUsername(!isEditingUsername);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+  };
 
   const handleLogout = () => {
     console.log('LOGOUT');
@@ -30,8 +73,26 @@ const Profile: React.FunctionComponent<RouteComponentProps> = ({ history }) => {
           <IonGrid fixed>
             <IonRow align-items-center justify-content-center>
               <IonCol style={{ textAlign: 'center', marginBottom: '8px' }} size="12">
+                {isEditingUsername ? (
+                  <IonItem>
+                    <IonLabel position="floating" color="primary">
+                      Nazwa użytkownia
+                    </IonLabel>
+                    <IonInput
+                      autofocus
+                      required
+                      placeholder="Nazwa użytkownika"
+                      name="username"
+                      value={username}
+                      onIonChange={e => setUsername((e.currentTarget as HTMLInputElement).value)}
+                    />
+                  </IonItem>
+                ) : (
+                  <IonText>
+                    <h1>{displayName}</h1>
+                  </IonText>
+                )}
                 <IonText>
-                  <h1>{displayName}</h1>
                   <p>{email}</p>
                 </IonText>
               </IonCol>
@@ -47,7 +108,15 @@ const Profile: React.FunctionComponent<RouteComponentProps> = ({ history }) => {
             </IonRow>
             <IonRow style={{ justifyContent: 'center' }}>
               <IonCol size="10">
-                <IonButton expand="block">Zmień nazwę</IonButton>
+                {isEditingUsername ? (
+                  <IonButton expand="block" onClick={handleUsernameChange} disabled={!usernameValid}>
+                    Akceptuj
+                  </IonButton>
+                ) : (
+                  <IonButton expand="block" onClick={changeUsername}>
+                    Zmień nazwę
+                  </IonButton>
+                )}
               </IonCol>
               <IonCol size="10">
                 <IonButton expand="block">Zmień avatar</IonButton>
