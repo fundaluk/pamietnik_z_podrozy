9f415dd27e10bbe824900454eb8413e3d76b2ea6
mrenok <50719241+mrenok@users.noreply.github.com>
2019-09-04 20:19:11 +0200

    Integracja Google Maps

--- src/pages/Add.tsx
+++ src/pages/Add.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
-import { useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { RouteComponentProps } from 'react-router-dom';
+import { Marker } from 'google-maps-react';
 
 import {
   IonHeader,
@@ -18,14 +19,65 @@ import {
   IonText,
 } from '@ionic/react';
 
+import MapInputField from '../components/MapInputField';
+
+import FirebaseContext from '../components/FirebaseContext';
+import UserContext from '../components/UserContext';
+
 const Add: React.FunctionComponent<RouteComponentProps> = ({ history }) => {
-  const [lat, setLat] = useState(0);
-  const [lng, setLng] = useState(0);
+  const [lat, setLat] = useState(50.06864775407978);
+  const [lng, setLng] = useState(19.955816843574212);
   const [name, setName] = useState('');
   const [description, setDescritpion] = useState('');
-  const [location, setLocation] = useState('');
+  const [place, setPlace] = useState('');
   const [invalid, setInvalid] = useState(true);
-  const [error, setError] = useState('');
+
+  const firebase = useContext(FirebaseContext);
+  const { user } = useContext(UserContext);
+
+  // Sprawdz czy formularz ok
+  useEffect(() => {
+    if (name !== '' && description !== '' && description.length <= 200) {
+      setInvalid(false);
+    } else {
+      setInvalid(true);
+    }
+  }, [name, description]);
+
+  const handleMapClick = (ref: any, map: any, event: any) => {
+    const location = event.latLng;
+    map.panTo(location);
+    setLat(location.lat());
+    setLng(location.lng());
+
+    // Sprawdź lokacje - Places API
+  };
+
+  // Dodaj miesce do bazy danych
+  const addPlace = async (event: Event) => {
+    event.preventDefault();
+
+    try {
+      const ref = await firebase.db.collection('places').add({
+        name,
+        description,
+        place,
+        location: new firebase.GeoPoint(lat, lng),
+        user: user.uid,
+        createdAt: firebase.timestamp,
+      });
+      // Przekieruje do tego miejsca - widok
+      history.push(`/places/place/${ref.id}`);
+    } catch (err) {}
+  };
+
+  const cancelAddingPlace = async (event: Event) => {
+    event.preventDefault();
+    setName('');
+    setDescritpion('');
+    setPlace('');
+    history.push('/places');
+  };
 
   return (
     <>
@@ -40,14 +92,15 @@ const Add: React.FunctionComponent<RouteComponentProps> = ({ history }) => {
             <IonCol size="12" style={{ marginBottom: '16px' }}>
               <IonItem>
                 <IonLabel position="floating" color="primary">
-                  Nazwa miejsca / Adres
+                  Nazwa miejsca
                 </IonLabel>
                 <IonInput
                   autofocus
                   required
-                  placeholder="Nazwa miejsca / adres"
+                  value={name}
+                  placeholder="Nazwa miejsca"
                   name="place-name"
-                  onIonChange={e => console.log(e.currentTarget)}
+                  onIonChange={e => setName((e.currentTarget as HTMLInputElement).value)}
                 />
               </IonItem>
             </IonCol>
@@ -56,30 +109,29 @@ const Add: React.FunctionComponent<RouteComponentProps> = ({ history }) => {
             <IonCol size="12" style={{ marginBottom: '16px' }}>
               <IonItem>
                 <IonLabel position="floating" color="primary">
-                  Opis miejsca (100 / 200 )
+                  Opis miejsca ({`${description.length}/200`})
                 </IonLabel>
                 <IonTextarea
                   required
+                  value={description}
                   placeholder="Opis miejsca"
                   name="place-description"
                   maxlength={200}
-                  onIonChange={e => console.log(e.currentTarget)}
+                  onIonChange={e => setDescritpion((e.currentTarget as HTMLInputElement).value)}
                 />
               </IonItem>
             </IonCol>
           </IonRow>
           <IonRow align-items-center justify-content-center>
-            <IonCol
-              offset="1"
-              size="10"
-              style={{ height: '40vh', backgroundColor: 'blue', marginBottom: '8px' }}
-            ></IonCol>
+            <IonCol offset="1" size="10" style={{ height: '40vh', marginBottom: '8px' }}>
+              <MapInputField onClick={handleMapClick} lat={lat} lng={lng} center={{ lat, lng }}>
+                <Marker position={{ lat, lng }} />
+              </MapInputField>
+            </IonCol>
           </IonRow>
           <IonRow align-items-center justify-content-center>
             <IonCol offset="1" size="10">
               <IonText>
-                <p>ul.Floriańska 1</p>
-                <p>Kraków</p>
                 <p>lat: {lat}</p>
                 <p>lng: {lng}</p>
               </IonText>
@@ -88,12 +140,12 @@ const Add: React.FunctionComponent<RouteComponentProps> = ({ history }) => {
 
           <IonRow align-items-center justify-content-center>
             <IonCol offset="1" size="10">
-              <IonButton style={{ marginTop: '8px' }} expand="block" onClick={() => console.log('add')}>
+              <IonButton style={{ marginTop: '8px' }} expand="block" onClick={addPlace} disabled={invalid}>
                 Dodaj miejsce
               </IonButton>
             </IonCol>
             <IonCol offset="1" size="10">
-              <IonButton expand="block" fill="clear" color="danger" onClick={() => console.log('add')}>
+              <IonButton expand="block" fill="clear" color="danger" onClick={cancelAddingPlace}>
                 ANULUJ
               </IonButton>
             </IonCol>
