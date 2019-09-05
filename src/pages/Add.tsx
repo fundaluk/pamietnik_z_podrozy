b3153903c2aea0b24cc9918bb8ba122814ff759d
tgadek <tomaszgadek92@wp.pl>
2019-09-05 12:16:17 +0200

    Wyswietlanie listy wszystkich miejsc

--- src/pages/Add.tsx
+++ src/pages/Add.tsx
@@ -1,7 +1,86 @@
-import React from 'react';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/react';
+import * as React from 'react';
+import { useContext, useEffect, useState } from 'react';
+import { RouteComponentProps } from 'react-router-dom';
+import { Marker } from 'google-maps-react';
 
-const Add: React.FunctionComponent = () => {
+import {
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonLabel,
+  IonItem,
+  IonInput,
+  IonTextarea,
+  IonButton,
+} from '@ionic/react';
+
+import MapInputField from '../components/MapInputField';
+
+import FirebaseContext from '../components/FirebaseContext';
+import UserContext from '../components/UserContext';
+
+const Add: React.FunctionComponent<RouteComponentProps> = ({ history }) => {
+  const [lat, setLat] = useState(50.06864775407978);
+  const [lng, setLng] = useState(19.955816843574212);
+  const [name, setName] = useState('');
+  const [description, setDescritpion] = useState('');
+  const [invalid, setInvalid] = useState(true);
+
+  const firebase = useContext(FirebaseContext);
+  const { user } = useContext(UserContext);
+
+  // Sprawdz czy formularz ok
+  useEffect(() => {
+    if (name !== '' && description !== '' && description.length <= 200) {
+      setInvalid(false);
+    } else {
+      setInvalid(true);
+    }
+  }, [name, description]);
+
+  const handleMapClick = (ref: any, map: any, event: any) => {
+    const location = event.latLng;
+    map.panTo(location);
+    setLat(location.lat());
+    setLng(location.lng());
+  };
+
+  // Dodaj miesce do bazy danych
+  const addPlace = async (event: Event) => {
+    event.preventDefault();
+
+    try {
+      const ref = await firebase.db.collection('places').add({
+        name,
+        description,
+        location: new firebase.GeoPoint(lat, lng),
+        user: user.uid,
+        createdAt: firebase.timestamp,
+      });
+      setName('');
+      setDescritpion('');
+      // Przekieruje do tego miejsca - widok
+      history.push(`/maps`);
+    } catch (err) {}
+  };
+
+  const cancelAddingPlace = async (event: Event) => {
+    event.preventDefault();
+    setName('');
+    setDescritpion('');
+    history.push('/places');
+  };
+
+  // Znajdz miejsca w pobliżu
+  const fetchPlaces = (mapProps: any, map: any) => {
+    const { google } = mapProps;
+    const service = new google.maps.places.PlacesService(map);
+  };
+
   return (
     <>
       <IonHeader>
@@ -9,7 +88,63 @@ const Add: React.FunctionComponent = () => {
           <IonTitle>Dodaj Miejsce</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent />
+      <IonContent>
+        <IonGrid>
+          <IonRow align-items-center justify-content-center>
+            <IonCol size="12" style={{ marginBottom: '16px' }}>
+              <IonItem>
+                <IonLabel position="floating" color="primary">
+                  Nazwa miejsca
+                </IonLabel>
+                <IonInput
+                  autofocus
+                  required
+                  value={name}
+                  placeholder="Nazwa miejsca"
+                  name="place-name"
+                  onIonChange={e => setName((e.currentTarget as HTMLInputElement).value)}
+                />
+              </IonItem>
+            </IonCol>
+          </IonRow>
+          <IonRow align-items-center justify-content-center>
+            <IonCol size="12" style={{ marginBottom: '16px' }}>
+              <IonItem>
+                <IonLabel position="floating" color="primary">
+                  Opis miejsca ({`${description.length}/200`})
+                </IonLabel>
+                <IonTextarea
+                  required
+                  value={description}
+                  placeholder="Opis miejsca"
+                  name="place-description"
+                  maxlength={200}
+                  onIonChange={e => setDescritpion((e.currentTarget as HTMLInputElement).value)}
+                />
+              </IonItem>
+            </IonCol>
+          </IonRow>
+          <IonRow align-items-center justify-content-center>
+            <IonCol offset="1" size="10" style={{ height: '40vh', marginBottom: '8px', padding: '0px' }}>
+              <MapInputField onClick={handleMapClick} lat={lat} lng={lng} center={{ lat, lng }} onReady={fetchPlaces}>
+                <Marker position={{ lat, lng }} />
+              </MapInputField>
+            </IonCol>
+          </IonRow>
+          <IonRow align-items-center justify-content-center>
+            <IonCol offset="1" size="10">
+              <IonButton style={{ marginTop: '8px' }} expand="block" onClick={addPlace} disabled={invalid}>
+                Dodaj miejsce
+              </IonButton>
+            </IonCol>
+            <IonCol offset="1" size="10">
+              <IonButton expand="block" fill="clear" color="danger" onClick={cancelAddingPlace}>
+                ANULUJ
+              </IonButton>
+            </IonCol>
+          </IonRow>
+        </IonGrid>
+      </IonContent>
     </>
   );
 };
--- src/pages/Maps.tsx
+++ src/pages/Maps.tsx
@@ -1,7 +1,40 @@
-import React from 'react';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/react';
+import * as React from 'react';
+import { useEffect, useContext, useState } from 'react';
+import { RouteComponentProps } from 'react-router-dom';
 
-const Maps: React.FunctionComponent = () => {
+import { useCollection } from 'react-firebase-hooks/firestore';
+
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonGrid, IonRow, IonCol, IonText } from '@ionic/react';
+
+import { Marker } from 'google-maps-react';
+
+import MapPlaces from '../components/MapPlaces';
+
+import FirebaseContext from '../components/FirebaseContext';
+import UserContext from '../components/UserContext';
+
+const Maps: React.FunctionComponent<RouteComponentProps> = ({ history }) => {
+  const firebase = useContext(FirebaseContext);
+  const { user } = useContext(UserContext);
+  // Pobierz wsztstkie mejsca dla jeednego usera
+  const [places, loading, error] = useCollection(firebase.db.collection('places').where('user', '==', user.uid), {
+    snapshotListenOptions: { includeMetadataChanges: true },
+  });
+
+  const [points, setPoints] = useState([]);
+
+  useEffect(() => {
+    if (places) {
+      const newPoints = places.docs.map(doc => {
+        const point = { lat: 0, lng: 0 };
+        point.lat = doc.data().location.latitude;
+        point.lng = doc.data().location.longitude;
+        return point;
+      });
+      setPoints(newPoints);
+    }
+  }, [places]);
+
   return (
     <>
       <IonHeader>
@@ -9,7 +42,47 @@ const Maps: React.FunctionComponent = () => {
           <IonTitle>Mapa Miejsc</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent />
+      <IonContent>
+        <IonGrid style={{ padding: '0px' }}>
+          <IonRow align-items-center justify-content-center>
+            <IonCol size="12" style={{ height: '100vh', padding: '0px' }}>
+              {points.length > 0 ? (
+                <MapPlaces points={points}>
+                  {places.docs.map(doc => (
+                    <Marker
+                      key={doc.id}
+                      position={{
+                        lat: doc.data().location.latitude,
+                        lng: doc.data().location.longitude,
+                      }}
+                    />
+                  ))}
+                </MapPlaces>
+              ) : (
+                <div
+                  style={{
+                    display: 'flex',
+                    alignItems: 'center',
+                    height: '100vh',
+                    padding: '8px 0',
+                    textAlign: 'center',
+                  }}
+                >
+                  <IonGrid fixed>
+                    <IonRow align-items-end justify-content-center>
+                      <IonCol align-self-center>
+                        <IonText color="primary">
+                          <p>Brak Miejsc</p>
+                        </IonText>
+                      </IonCol>
+                    </IonRow>
+                  </IonGrid>
+                </div>
+              )}
+            </IonCol>
+          </IonRow>
+        </IonGrid>
+      </IonContent>
     </>
   );
 };
